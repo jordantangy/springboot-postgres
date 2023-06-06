28b4a378e707e44e655dd19ebb7d18cd63345b38
Jordan Tangy <49556564+jordantangy@users.noreply.github.com>
2023-06-06 22:14:52 +0300

    Update TokenCleanUpScheduler.java

--- src/main/java/com/example/springbootpostgres/token/TokenCleanUpScheduler.java
+++ src/main/java/com/example/springbootpostgres/token/TokenCleanUpScheduler.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-//package com.example.springbootpostgres.token;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.springframework.scheduling.annotation.Scheduled;
-//
-//
-//@Configuration
-//@EnableScheduling
-//public class TokenCleanUpScheduler {
-//
-//    @Autowired
-//    private TokenRepository tokenRepository;
-//
-//    @Scheduled(fixedRate = 3600000) // run once per hour
-//    public void deleteExpiredTokens() {
-//
-//        for (Token token : tokenRepository.findAll()) {
-//            if (token.isExpired() == true) {
-//                tokenRepository.delete(token);
-//            }
-//        }
-//    }
-//}
-=======
 package com.example.springbootpostgres.token;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,4 +23,4 @@ public class TokenCleanUpScheduler {
         }
     }
 }
->>>>>>> cfc51bb (schedule expired tokens removal)
+
